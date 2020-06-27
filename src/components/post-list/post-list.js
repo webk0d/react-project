e95d05d35aeadae544843645a0fc0e15195fbf69
SergeyChumakov <webk0d@ya.ru>
2020-06-27 18:16:00 +0500

    added denim likes and stars

--- src/components/post-list/post-list.js
+++ src/components/post-list/post-list.js
@@ -3,12 +3,31 @@ import PostListItem from '../post-list-item';
 
 import './post-list.css'
 
-const PostList = () => {
+const PostList = ({posts}) => {
+
+    const elements = posts.map((item) => {
+        if (typeof item === 'object' && isEmpty(item) ) {
+            const {id, ...itemProps} = item;
+            
+            return (
+                <li key={id} className='list-group-item'>
+                    <PostListItem {...itemProps} />
+                </li>
+            )
+        }
+    })
+
+    function isEmpty(obj) {
+        for(let key in obj)
+        {
+            return true;
+        }
+        return false;
+    }
+
     return (
         <ul className="app-list list-group">
-            <PostListItem/>
-            <PostListItem/>
-            <PostListItem/>
+            {elements}
         </ul>
     )
 }
